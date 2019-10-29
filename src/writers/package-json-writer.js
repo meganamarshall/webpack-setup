6d041489ad3277c9589d974be681710b94fcde6b
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-10-29 10:24:47 -0700

    package json writer

---
+++ src/writers/package-json-writer.js
@@ -0,0 +1,35 @@
+const { writeJSON } = require('../writer');
+
+const packageObj = {
+  name: 'starter-code',
+  version: '1.0.0',
+  description: '',
+  main: 'src/index.js',
+  jest: {
+    setupFilesAfterEnv: [
+      '<rootDir>src/setupTests.js'
+    ],
+    snapshotSerializers: [
+      'enzyme-to-json/serializer'
+    ],
+    moduleNameMapper: {
+      '\\.(css)$': 'identity-obj-proxy',
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js'
+    }
+  },
+  scripts: {
+    test: 'jest --verbose',
+    'test:watch': 'npm run test -- --watch',
+    start: 'webpack-dev-server --hot --mode development --devtool eval-source-map',
+    build: 'webpack -p --devtool source-map'
+  },
+  keywords: [],
+  author: '',
+  license: 'ISC',
+};
+
+const packageJsonWriter = (path) => {
+  return writeJSON(packageObj, path);
+};
+
+module.exports = packageJsonWriter;
