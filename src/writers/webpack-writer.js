a4b62d45cfc3e42ae8bad996bb54fcd91f128611
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-11-04 19:07:07 -0800

    tweaking webpack config

--- src/writers/webpack-writer.js
+++ src/writers/webpack-writer.js
@@ -7,10 +7,13 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: './bundle.[hash].js'
+    filename: './bundle.[hash].js',
+    publicPath: '/'
   },
   devServer: {
-    port: 7890
+    port: 7890,
+    ,
+    historyApiFallback: true
   },
   plugins: [
     new HtmlPlugin({ template: './src/index.html'}),
